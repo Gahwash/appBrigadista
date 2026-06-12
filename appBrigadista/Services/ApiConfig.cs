7cccf8ae7741fcce311ea7b1c8fb83381b680788
Gahwash <taquito011@gmail.com>
2026-06-11 19:41:53 -0600

    Agrega autenticacion de ususarios y pantalla de login, ip global

--- appBrigadista/Services/ApiConfig.cs
+++ appBrigadista/Services/ApiConfig.cs
@@ -7,7 +7,7 @@ namespace appBrigadista.Services
     public class ApiConfig
     {
         // Solo cambias esta IP cuando cambie el nodo FOG
-        public const string Host = "192.168.10.229";
+        public const string Host = "192.168.1.20";
 
         public const int ApiPort = 8080;
         public const int MqttPort = 1883;
